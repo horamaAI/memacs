c93f9f0b14f4d3abd338bdba2f219748cbb40f6d
myfirstdebianpc <rurayi@yahoo.fr>
2024-05-11 01:56:36 +0200

    REFACT000: updating emacs init file

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
 
-(org-babel-load-file (expand-file-name "init_configs.org" user-emacs-directory))
+;; loads configs from org-file 'init_configs.org'
+;; needed to use it with local emacs:
+;; 1. create symbolic link to this initialization file: ~ln -s $MEMACS/init.el ~/.emacs.d/~
+;; 2. create symbolic link to 'init_configs.el' (~ln -s $MEMACS/init_configs.el ~/.emacs.d/~). 'init_configs.el' is generated after evaluating this initialization file
+(org-babel-load-file (substitute-in-file-name "$MEMACS/init_configs.org"))
 
